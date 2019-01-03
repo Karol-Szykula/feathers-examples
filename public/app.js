6df6e60c656a86ea92e8af01da9b08de3fa91d2a
Karol-Szykula <karol.szykula@wp.pl>
2019-01-03 03:08:54 +0100

    Refactor front client to ES6

--- public/app.js
+++ public/app.js
@@ -1,19 +1,43 @@
 
-const buttonGetWeatherApi = document.createElement("button");
-buttonGetWeatherApi.innerHTML = "Get weather";
+class App {
+    constructor(containerName) {
+        this.container = document.querySelector(`.${containerName}`) || document.body
+    }
 
-const container = document.querySelector('.container')
-container.innerHTML = ''
-container.appendChild(buttonGetWeatherApi)
+    render() {
+        // const container = document.querySelector('.container')
+        this.container.innerHTML = ''
 
-// 3. Add event handler
-buttonGetWeatherApi.addEventListener("click", function () {
-    fetch('http://localhost:3030//api')
-        .then(response => { return response.json() })
-        .then((json) => { console.log(json) })
-        .catch(error => { console.log(error) })
-});
+        const buttonGetWeatherApi = document.createElement("button");
+        buttonGetWeatherApi.innerHTML = "Get weather";
 
-function showWeather(whereDiv, json) {
-    whereDiv.innerHTML = json
+        const divButton = document.createElement('div')
+        divButton.innerHTML = ''
+        divButton.appendChild(buttonGetWeatherApi)
+
+        const divWeather = document.createElement('div')
+        divWeather.innerHTML = ''
+
+
+        // 3. Add event handler
+        buttonGetWeatherApi.addEventListener("click", function () {
+            fetch('http://localhost:3030//api')
+                .then(response => { return response.json() })
+                .then((json) => { this.render(divWeather, JSON.stringify(json, null, 2)) })
+                .catch(error => { console.log(error) })
+        });
+
+        this.container.appendChild(divButton)
+        this.container.appendChild(divWeather)
+
+        this.container.appendChild(buttonGetWeatherApi)
+    }
+
+    renderWeather(where, what) {
+        where.innerHTML = ''
+        where.innerHTML = what
+    }
 }
+
+const app = new App('container')
+app.render()
